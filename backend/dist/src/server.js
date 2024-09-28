ca042916ba03ef7856a16208c9ea1da36f257f28
Jorge H Frasson <jorge.frasson147@gmail.com>
2024-09-28 11:07:52 -0300

    Ajusta gitignore

--- backend/dist/src/server.js
+++ backend/dist/src/server.js
@@ -6,55 +6,24 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const fastify_1 = __importDefault(require("fastify"));
 const user_controller_1 = require("./controller/user.controller");
 const auth_controller_1 = require("./controller/auth.controller");
-const env_1 = __importDefault(require("@fastify/env"));
 const dotenv_1 = require("dotenv");
 // Carregar variáveis de ambiente
 (0, dotenv_1.configDotenv)();
 const PORT = Number(process.env.PORT) || 4000;
-const schema = {
-    type: 'object',
-    required: ['DATABASE_URL', 'SECRET'],
-    properties: {
-        DATABASE_URL: {
-            type: 'string',
-            default: ''
-        },
-        PORT: {
-            type: 'string',
-            default: 3000
-        },
-        SECRET: {
-            type: 'string',
-            default: 'defaultsupersecret'
-        },
-    }
-};
-const options = {
-    dotenv: true,
-    schema: schema,
-    data: process.env
-};
 // Mova a inicialização do servidor para uma função assíncrona
-async function start() {
+function start() {
     // Set Fastify APP
     const app = (0, fastify_1.default)({ logger: true });
-    // Set Environment variables
-    app.register(env_1.default, options);
-    await app.after(); // await dentro da função
     // Controllers
     app.register(user_controller_1.UserController, { prefix: '/users' });
     app.register(auth_controller_1.AuthController, { prefix: '/auth' });
-    // Servier configuration
-    app.listen({ port: PORT }, (err, address) => {
-        if (err) {
-            console.error(err);
-            process.exit(1);
-        }
-        console.log(`Server is running on ${address}`);
-    });
+    return app;
 }
-// Iniciar o servidor
-start().catch((err) => {
-    console.error('Error starting the server:', err);
-    process.exit(1);
+const app = start();
+app.listen({ port: PORT }, (err, address) => {
+    if (err) {
+        console.error(err);
+        process.exit(1);
+    }
+    console.log(`Server is running on ${address}`);
 });
